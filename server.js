6fc91d09b6a52fb784d5051322f40a354f7ca668
Your Name <you@example.com>
2025-03-07 23:08:46 -0800

    statics fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-// const static = require("./routes/static")
+const static = require("./routes/static")
 
 /* ***********************
  * View Engine and Templates
@@ -21,7 +21,7 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-//app.use(static)
+app.use(static)
 // Index route
 app.get("/", function(req, res){
 res.render("index", {title: "Home"})
