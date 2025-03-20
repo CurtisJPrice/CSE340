b5afea4e29375f7ba62db0721f6be965473f049f
Your Name <you@example.com>
2025-03-20 13:07:19 -0700

    reverting, to see errors for Render.com

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,47 +1,39 @@
--- Query 1
-INSERT INTO public.account (
-    acc_firstname,
-    acc_lastname,
-    acc_email,
-    acc_password
-  )
-VALUES (
-    'Tony',
-    'Stark',
-    'tony@starkent.com',
-    'Iam1ronM@n'
-  );
--- Query 2
-SELECT *
-FROM public.account;
-UPDATE public.account
-SET acc_type = 'Admin'
-WHERE acc_id = 1;
--- Query 3
-DELETE FROM public.account
-WHERE acc_id = 1;
--- Query 4
-SELECT *
-FROM public.inventory
-WHERE inv_model = 'Hummer';
+INSERT INTO account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (Tony, Stark, tony @starkent.com, Iam1ronM @n);
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony'
+    AND account_lastname = 'Stark';
+DELETE FROM account
+WHERE account_firstname = 'Tony'
+    AND account_lastname = 'Stark';
 UPDATE inventory
 SET inv_description = REPLACE(
-    inv_description,
-    'small interior',
-    'huge interior'
-  )
-WHERE inv_model = 'Hummer';
--- Query 5
-SELECT inv_make,
-  inv_model,
-  clas_name
+        inv_description,
+        'a huge interior',
+        'small interiors'
+    )
+WHERE inv_make = 'GM'
+    AND inv_model = 'Hummer';
+SELECT inventory.inv_make,
+    inventory.inv_model,
+    classification.classification_name
 FROM inventory
-  INNER JOIN classification ON inventory.clas_id = classification.clas_id
-WHERE clas_name = 'Sport';
--- Query 6
-SELECT inv_image,
-  inv_thumbnail
-FROM inventory;
+    INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
 UPDATE inventory
-SET inv_image = REPLACE(inv_image, 'images/', 'images/vehicles/'),
-  inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicles/');
+SET inv_image = CONCAT(
+        LEFT(inv_image, 7),
+        '/vehicles',
+        (SUBSTRING(inv_image, 8))
+    ),
+    inv_thumbnail = CONCAT(
+        LEFT(inv_thumbnail, 7),
+        '/vehicles',
+        (SUBSTRING(inv_thumbnail, 8))
+    );
