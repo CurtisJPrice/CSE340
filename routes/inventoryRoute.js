bb7d5594158706c214ebedfb9a210771bcf7d8f9
Your Name <you@example.com>
2025-03-28 17:08:30 -0700

    fixes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,7 +2,7 @@
 
 const express = require("express")
 const router = express.Router()
-const invController = require("../controllers/inventoryController")
+const invController = require("../controllers/invController")
 const utilities = require("../utilities/")  // Import utilities for error handling
 
 // Simulate routes without database queries
