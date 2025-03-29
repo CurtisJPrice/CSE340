2c4124c33b80c4ecb63da811f50b1dd146b850e7
Your Name <you@example.com>
2025-03-28 17:26:16 -0700

    fixes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const invController = require("../controllers/invController");
 
 // Example route for "by classification id"
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get('/type/:classificationId', async (req, res) => {
+    // Do something
+    res.render('someView');
+});
+
 
 module.exports = router;
