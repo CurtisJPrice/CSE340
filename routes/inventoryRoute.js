55effa5e169bfe6739b95ee57bd6986ed9ecdfe1
Your Name <you@example.com>
2025-03-28 17:13:36 -0700

    fix

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -8,6 +8,12 @@ const utilities = require("../utilities/")  // Import utilities for error handli
 // Simulate routes without database queries
 
 // Example route for "by classification id"
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+router.get("/type/:classificationId", async (req, res, next) => {
+    try {
+      await invController.buildByClassificationId(req, res, next);  // Make sure the controller method is async
+    } catch (error) {
+      next(error);  // Make sure to pass the error to the next middleware
+    }
+});
 
 module.exports = router;
