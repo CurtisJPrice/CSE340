93a129406a4fbaba4d50b1cdf019ca891a959bf7
Your Name <you@example.com>
2025-04-05 17:19:08 -0700

    fixes

--- public/js/inv-update.js
+++
@@ -1,6 +0,0 @@
-const form = document.querySelector("#updateForm");
-form.addEventListener("change", function () {
-  const updateBtn = document.querySelector("button");
-  updateBtn.removeAttribute("disabled");
-  updateBtn.classList.add("formbutton");
-});
